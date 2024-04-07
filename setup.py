7bf17862cb0bb853add1d71d17226cf0f274bdd2
AyushSingh096 <ayushsingh00112233@gmail.com>
2024-04-08 01:24:24 +0530

    Notebook experiment

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "mcqgenerator",
+    version = "0.0.1",
+    author = "ayush singh",
+    author_email = "ayushsingh0011223@gmail.com",
+    install_requires = [],
+    packages = find_packages()
+)
